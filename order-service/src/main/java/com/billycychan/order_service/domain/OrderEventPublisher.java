e1226db22d1e8f21e9df22f457f4d658c0f70391
Billy Chan <billycychan.work@gmail.com>
2024-11-03 22:42:11 -0500

    feat(publish-order-event): publish order created event using outbox pattern

---
+++ order-service/src/main/java/com/billycychan/order_service/domain/OrderEventPublisher.java
@@ -0,0 +1,39 @@
+package com.billycychan.order_service.domain;
+
+import com.billycychan.order_service.ApplicationProperties;
+import com.billycychan.order_service.domain.models.OrderCancelledEvent;
+import com.billycychan.order_service.domain.models.OrderCreatedEvent;
+import com.billycychan.order_service.domain.models.OrderDeliveredEvent;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+class OrderEventPublisher {
+    private final RabbitTemplate rabbitTemplate;
+    private final ApplicationProperties properties;
+
+    OrderEventPublisher(RabbitTemplate rabbitTemplate, ApplicationProperties properties) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.properties = properties;
+    }
+
+    public void publish(OrderCreatedEvent event) {
+        this.send(properties.newOrdersQueue(), event);
+    }
+
+    public void publish(OrderDeliveredEvent event) {
+        this.send(properties.deliveredOrdersQueue(), event);
+    }
+
+    public void publish(OrderCancelledEvent event) {
+        this.send(properties.cancelledOrdersQueue(), event);
+    }
+
+    public void publish(OrderErrorEvent event) {
+        this.send(properties.errorOrdersQueue(), event);
+    }
+
+    private void send(String routingKey, Object payload) {
+        rabbitTemplate.convertAndSend(properties.orderEventsExchange(), routingKey, payload);
+    }
+}
