34540ee25e2df9e7b8b8ccea751dbda7fdddeaed
Billy Chan <billycychan.work@gmail.com>
2024-12-19 14:20:26 +0800

    feat: add webapp starter

---
+++ bookstore-webapp/src/test/java/com/billycychan/webapp/BookstoreWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billycychan.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
